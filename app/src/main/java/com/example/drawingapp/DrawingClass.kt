7a98f64c64a18a13fb276209c8590e3cc2c88c4f
dkwns <kaandogan2013@gmail.com>
2023-05-21 04:32:09 +0300

    DrawingView with my implement.

--- app/src/main/java/com/example/drawingapp/DrawingClass.kt
+++ app/src/main/java/com/example/drawingapp/DrawingClass.kt
@@ -6,35 +6,63 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
+import android.icu.text.ListFormatter.Width
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
-class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
-    private lateinit var canvas: Canvas
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private lateinit var drawingCanvas: Canvas
     private lateinit var canvasBitmap: Bitmap
-    private lateinit var drawPaint: Paint
-    private lateinit var canvasPaint: Paint
-    private var brushSize: Float = 0f
-    private var brushColor = Color.BLACK
-    private lateinit var drawPath: CustomPath
-    internal inner class CustomPath(brushColor: Int, brushSize: Float) : Path() {
-
+    private var paint: Paint = Paint(Paint.DITHER_FLAG).apply {
+        color = Color.BLACK
+        style = Paint.Style.FILL_AND_STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
     }
-     init {
-         setUpDrawing()
-     }
+    private lateinit var path: Path
 
-    private fun setUpDrawing() {
-        canvas = Canvas()
-        canvasPaint = Paint()
-        drawPaint = Paint()
-        drawPaint.apply {
-            color = brushColor
-            style = Paint.Style.STROKE
-            strokeJoin = Paint.Join.ROUND
-            strokeCap = Paint.Cap.ROUND
+    init {
+        path = Path()
+    }
+
+    private fun setUpDrawing(w: Int, h: Int) {
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        drawingCanvas = Canvas(canvasBitmap)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(canvasBitmap, 0f, 0f, paint)
+        canvas.drawPath(path, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                path.moveTo(x, y)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                path.lineTo(x, y)
+            }
+            MotionEvent.ACTION_UP -> {
+                drawingCanvas.drawPath(path, paint)
+                path.reset()
+            }
         }
-        drawPath = CustomPath(brushColor, brushSize)
-        brushSize = 20f
+        invalidate()
+        return true
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        if (w > 0 && h > 0) {
+            setUpDrawing(w, h)
+        }
     }
 }
+
--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -7,9 +7,10 @@ import com.example.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+        val drawingView = binding.drawingView
     }
 }
